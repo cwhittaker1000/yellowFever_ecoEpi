37d865f2f6a15e28f236f3e869867162e3c7436b
CharlieWhittaker <cwhittaker1000@gmail.com>
2024-04-02 13:23:56 +0100

    offset gamma and gamma in

--- analyses/0_IBM_inputEstimation/2_fitDistribution_EIPBatesData.R
+++ analyses/0_IBM_inputEstimation/2_fitDistribution_EIPBatesData.R
@@ -56,89 +56,124 @@ data_stan_adults <- list(N = length(degrees30_df_adults$days),
                          b_2 = 10,
                          min_p_death_prior_mean = 0.04,
                          min_p_death_prior_sd = 0.02,
-                         max_p_death_prior_mean = 0.6,
+                         max_p_death_prior_mean = 0.5,
                          max_p_death_prior_sd = 0.1)
 fit_adults <- sampling(model_gamma, data = data_stan_adults, iter = 2000, chains = 4)
 summary(fit_adults)
 
 ## Plotting
 par(mfrow = c(2, 2))
-hist(rstan::extract(fit_babies, "days_simulated")[[1]], breaks = 50)
+
 mean(rstan::extract(fit_babies, "days_simulated")[[1]])
 pmin_babies <- mean(rstan::extract(fit_babies, "p_death_min")[[1]])
 pmax_babies <- mean(rstan::extract(fit_babies, "p_death_max")[[1]])
 degrees30_df_babies$perc_mod <- (degrees30_df_babies$perc - pmin_babies) / (pmax_babies - pmin_babies)
-plot(degrees30_df_babies$days, degrees30_df_babies$perc_mod, ylim = c(0, 1), type = "l")
 x <- seq(0, 20, by = 0.01)
 modelled_a <- mean(rstan::extract(fit_babies, "a")[[1]])
 modelled_b <- mean(rstan::extract(fit_babies, "b")[[1]])
 cdf_values <- pgamma(x, shape = modelled_a, rate = modelled_b)
-lines(x, cdf_values, type = "l", col = "red")
+plot(degrees30_df_babies$days, degrees30_df_babies$perc, ylim = c(0, 1), type = "l",
+     xlab = "Time (Days)", ylab = "Mouse Mortality", main = "Mouse Mortality")
+death_calc <- pmin_babies + pgamma(x, shape = modelled_a, rate = modelled_b) * (pmax_babies - pmin_babies)
+lines(x, death_calc, col = "red")
+lines(x, rep(pmin_babies, times = length(x)), lty = 2)
+hist(rstan::extract(fit_babies, "days_simulated")[[1]], breaks = 50, xlim = c(0, 30), xlab = "Time (Days)", main = "EIP Distribution")
+lines(x, 400 * cdf_values, col = "red")
 
-hist(rstan::extract(fit_adults, "days_simulated")[[1]], breaks = 50)
 mean(rstan::extract(fit_adults, "days_simulated")[[1]])
 pmin_adults <- mean(rstan::extract(fit_adults, "p_death_min")[[1]])
 pmax_adults <- mean(rstan::extract(fit_adults, "p_death_max")[[1]])
 degrees30_df_adults$perc_mod <- (degrees30_df_adults$perc - pmin_adults) / (pmax_adults - pmin_adults)
-plot(degrees30_df_adults$days, degrees30_df_adults$perc_mod, ylim = c(0, 1.5), type = "l")
 x <- seq(0, 20, by = 0.01)
 modelled_a <- mean(rstan::extract(fit_adults, "a")[[1]])
 modelled_b <- mean(rstan::extract(fit_adults, "b")[[1]])
 cdf_values <- pgamma(x, shape = modelled_a, rate = modelled_b)
-lines(x, cdf_values, type = "l", col = "blue")
-
-
-
+plot(degrees30_df_adults$days, degrees30_df_adults$perc, ylim = c(0, 1), type = "l",
+     xlab = "Time (Days)", ylab = "Mouse Mortality", main = "Mouse Mortality")
+death_calc <- pmin_adults + pgamma(x, shape = modelled_a, rate = modelled_b) * (pmax_adults - pmin_adults)
+lines(x, death_calc, col = "blue")
+lines(x, rep(pmin_adults, times = length(x)), lty = 2)
+hist(rstan::extract(fit_adults, "days_simulated")[[1]], breaks = 50, xlim = c(0, 30), xlab = "Time (Days)", main = "EIP Distribution")
+lines(x, 350 * cdf_values, col = "blue")
 
 #############
 
-# Stan model running
-model <- stan_model("analyses/0_IBM_inputEstimation/models/EIP_Offsetgamma.stan")
-data_stan <- list(N = length(degrees30_df_babies$days),
-                  day = degrees30_df_babies$days,
-                  infected = degrees30_df_babies$total,
-                  died = degrees30_df_babies$mod,
-                  a_1 = 0.01,
-                  a_2 = 10,
-                  b_1 = 0.01,
-                  b_2 = 10,
-                  offset_mean = 2)
-fit <- sampling(model, data=data_stan, iter=2000, chains=4)
-summary(fit)
-print(fit)
-pairs(fit)
-hist(rstan::extract(fit, "a")[[1]], breaks = 50)
-hist(rstan::extract(fit, "b")[[1]], breaks = 50)
-hist(rstan::extract(fit, "days_simulated")[[1]], breaks = 100, xlim = c(0, 20))
+### Fitting gamma to babies and adult data
+model_gamma_offset <- stan_model("analyses/0_IBM_inputEstimation/models/EIP_Offsetgamma.stan")
 
-plot(degrees30_df_babies$days, degrees30_df_babies$perc_mod, ylim = c(0, 1), type = "l")
+## Babies fit
+data_stan_babies_offset <- list(N = length(degrees30_df_babies$days),
+                                day = degrees30_df_babies$days,
+                                infected = degrees30_df_babies$total,
+                                died = degrees30_df_babies$mod,
+                                a_1 = 0.1,
+                                a_2 = 10,
+                                b_1 = 0.1,
+                                b_2 = 10,
+                                min_p_death_prior_mean = 0.33,
+                                min_p_death_prior_sd = 0.02,
+                                max_p_death_prior_mean = 1,
+                                max_p_death_prior_sd = 0.1,
+                                offset_mean = 2,
+                                offset_sd = 0.2)
+fit_babies_offset <- sampling(model_gamma_offset, data = data_stan_babies_offset, iter = 2000, chains = 4)
+print(fit_babies_offset)
+
+## Adults fit
+data_stan_adults_offset <- list(N = length(degrees30_df_adults$days),
+                                day = degrees30_df_adults$days,
+                                infected = degrees30_df_adults$total,
+                                died = degrees30_df_adults$mod,
+                                a_1 = 0.1,
+                                a_2 = 10,
+                                b_1 = 0.1,
+                                b_2 = 10,
+                                min_p_death_prior_mean = 0.04,
+                                min_p_death_prior_sd = 0.02,
+                                max_p_death_prior_mean = 0.5,
+                                max_p_death_prior_sd = 0.1,
+                                offset_mean = 2,
+                                offset_sd = 0.2)
+fit_adults_offset <- sampling(model_gamma_offset, data = data_stan_adults_offset, iter = 2000, chains = 4)
+print(fit_adults_offset)
+
+## Plotting
+par(mfrow = c(2, 2))
+
+mean(rstan::extract(fit_babies_offset, "days_simulated")[[1]])
+pmin_babies <- mean(rstan::extract(fit_babies_offset, "p_death_min")[[1]])
+pmax_babies <- mean(rstan::extract(fit_babies_offset, "p_death_max")[[1]])
+degrees30_df_babies$perc_mod <- (degrees30_df_babies$perc - pmin_babies) / (pmax_babies - pmin_babies)
 x <- seq(0, 20, by = 0.01)
-modelled_a <- mean(rstan::extract(fit, "a")[[1]])
-modelled_b <- mean(rstan::extract(fit, "b")[[1]])
-modelled_offset <- mean(rstan::extract(fit, "offset")[[1]])
+modelled_a <- mean(rstan::extract(fit_babies_offset, "a")[[1]])
+modelled_b <- mean(rstan::extract(fit_babies_offset, "b")[[1]])
+modelled_offset <- mean(rstan::extract(fit_babies_offset, "offset")[[1]])
 cdf_values <- pgamma(x, shape = modelled_a, rate = modelled_b)
-lines(x + modelled_offset, cdf_values, type = "l", col = "red")
+plot(degrees30_df_babies$days, degrees30_df_babies$perc, ylim = c(0, 1), type = "l",
+     xlab = "Time (Days)", ylab = "Mouse Mortality", main = "Mouse Mortality")
+death_calc <- pmin_babies + pgamma(x, shape = modelled_a, rate = modelled_b) * (pmax_babies - pmin_babies)
+lines(x + modelled_offset, death_calc, col = "red")
+lines(x, rep(pmin_babies, times = length(x)), lty = 2)
+hist(rstan::extract(fit_babies_offset, "days_simulated")[[1]], breaks = 50, xlim = c(0, 30), xlab = "Time (Days)", main = "EIP Distribution")
+lines(x + modelled_offset, 1000 * cdf_values, col = "red")
 
-# Stan model running
-model2 <- stan_model("analyses/0_IBM_inputEstimation/models/EIP_weibull.stan")
-data_stan <- list(N = length(degrees30_df_babies$days),
-                  day = degrees30_df_babies$days,
-                  infected = degrees30_df_babies$total,
-                  died = degrees30_df_babies$mod,
-                  a_1 = 1,
-                  a_2 = 10,
-                  b_1 = 1,
-                  b_2 = 10)
-fit2 <- sampling(model2, data=data_stan, iter=2000, chains=1)
-summary(fit2)
-hist(rstan::extract(fit2, "a")[[1]], breaks = 50)
-hist(rstan::extract(fit2, "b")[[1]], breaks = 50)
-hist(rstan::extract(fit2, "days_simulated")[[1]], breaks = 50)
-
-plot(degrees30_df_babies$days, degrees30_df_babies$perc_mod, ylim = c(0, 1), type = "l")
+mean(rstan::extract(fit_adults_offset, "days_simulated")[[1]])
+pmin_adults <- mean(rstan::extract(fit_adults_offset, "p_death_min")[[1]])
+pmax_adults <- mean(rstan::extract(fit_adults_offset, "p_death_max")[[1]])
+degrees30_df_adults$perc_mod <- (degrees30_df_adults$perc - pmin_adults) / (pmax_adults - pmin_adults)
 x <- seq(0, 20, by = 0.01)
-modelled_a <- mean(rstan::extract(fit2, "a")[[1]])
-modelled_b <- mean(rstan::extract(fit2, "b")[[1]])
-cdf_values <- pweibull(x, shape = modelled_a, scale = modelled_b)
-lines(x, cdf_values, type = "l", col = "red")
+modelled_a <- mean(rstan::extract(fit_adults_offset, "a")[[1]])
+modelled_b <- mean(rstan::extract(fit_adults_offset, "b")[[1]])
+modelled_offset <- mean(rstan::extract(fit_adults_offset, "offset")[[1]])
+cdf_values <- pgamma(x, shape = modelled_a, rate = modelled_b)
+plot(degrees30_df_adults$days, degrees30_df_adults$perc, ylim = c(0, 1), type = "l",
+     xlab = "Time (Days)", ylab = "Mouse Mortality", main = "Mouse Mortality")
+death_calc <- pmin_adults + pgamma(x, shape = modelled_a, rate = modelled_b) * (pmax_adults - pmin_adults)
+lines(x + modelled_offset, death_calc, col = "blue")
+lines(x, rep(pmin_adults, times = length(x)), lty = 2)
+hist(rstan::extract(fit_adults_offset, "days_simulated")[[1]], breaks = 50, xlim = c(0, 30), xlab = "Time (Days)", main = "EIP Distribution")
+lines(x + modelled_offset, 500 * cdf_values, col = "blue")
+
+#############
+
 
